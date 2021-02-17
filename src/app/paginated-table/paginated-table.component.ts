571bfc366142e1d2d78159debc21da64167ecffe
Joseba Andres <>
2021-02-17 08:23:28 +0100

    Propagate columns

--- src/app/paginated-table/paginated-table.component.ts
+++ src/app/paginated-table/paginated-table.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChildren, Input, QueryList } from '@angular/core';
+import { TableColumnDirective } from '../table-column/table-column.directive';
 
 @Component({
   selector: 'app-paginated-table',
   templateUrl: './paginated-table.component.html',
-  styleUrls: ['./paginated-table.component.scss']
+  styleUrls: ['./paginated-table.component.scss'],
 })
-export class PaginatedTableComponent implements OnInit {
+export class PaginatedTableComponent {
+  @Input() data: Array<any> | undefined;
 
-  constructor() { }
+  @ContentChildren(TableColumnDirective)
+  columns: QueryList<TableColumnDirective> | undefined;
 
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
