33fc58d5f2e7798d2249a5acff6f1a2e21684936
Joseba Andres <>
2021-02-17 08:11:07 +0100

    Add paginated table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { TableCellComponent } from './table-cell/table-cell.component';
 import { TableColumnDirective } from './table-column/table-column.directive';
+import { PagerComponent } from './pager/pager.component';
+import { PaginatedTableComponent } from './paginated-table/paginated-table.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { TableColumnDirective } from './table-column/table-column.directive';
     TableComponent,
     TableColumnDirective,
     TableCellComponent,
+    PagerComponent,
+    PaginatedTableComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/paginated-table/paginated-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paginated-table',
+  templateUrl: './paginated-table.component.html',
+  styleUrls: ['./paginated-table.component.scss']
+})
+export class PaginatedTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
